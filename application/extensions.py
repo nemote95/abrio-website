720b1f1bc1b8e4fddec9b9cb80102179cb8b478b
nemote95 <nemote95@gmail.com>
2016-03-17 06:48:44 +0330

    server-side session replacemen

--- application/extensions.py
+++ application/extensions.py
@@ -1,9 +1,15 @@
 # flask imports
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
+import redis
+from flask_kvsession import KVSessionExtension
+from simplekv.memory.redisstore import RedisStore
 
-__all__ = ["db", "login_manager"]
+__all__ = ["db", "login_manager","kvsession"]
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
+
+store = RedisStore(redis.StrictRedis())
+kvsession = KVSessionExtension(store)
