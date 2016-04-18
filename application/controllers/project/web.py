afcd882c7fd5ec0eec3e43c51842ecbfbfb6898b
nemote95 <nemote95@gmail.com>
2016-04-18 23:19:48 +0430

    simple html js to use define_logic api

--- application/controllers/project/web.py
+++ application/controllers/project/web.py
@@ -8,7 +8,7 @@ from flask.ext.login import current_user, login_required
 from application.models.project import Project
 from application.models.component import Component
 from application.models.logic import Logic
-from application.forms.project import CreateProjectForm, LogicForm
+from application.forms.project import CreateProjectForm
 from application.extensions import db, redis
 from application.decorators import permission
 
@@ -40,42 +40,13 @@ def create():
 @login_required
 @permission(Project, 'pid')
 def view(pid, obj=None):
-    logic_form=LogicForm(request.form)
     components_choices = [(c.id, c.name) for c in Component.query.filter(
         or_(Component.owner_id == current_user.id, Component.private == False)).all()]
-    logic_form.component2.choices = components_choices
-    logic_form.component1.choices = components_choices
     project_logic = Logic.query.filter_by(project_id=pid).all()
     logic_view = [(Component.query.filter_by(id=l.component_1_id).one_or_none(),
                    Component.query.filter_by(id=l.component_2_id).one_or_none(), l.message_type) for l in project_logic]
     running = redis.exists('abr:%s' % obj.private_key)
-    return render_template('project/view.html', project=obj, logic_view=logic_view,logic_form=logic_form,
-                           components_choices=components_choices, running=running)
-
-
-@project.route('/define_logic/<int:pid>', methods=['POST'])
-@login_required
-@permission(Project, 'pid')
-def define_logic(pid, obj=None):
-    logic_form = LogicForm(request.form)
-    components_choices = [(c.id, c.name) for c in Component.query.filter(
-        or_(Component.owner_id == current_user.id, Component.private == False)).all()]
-    logic_form.component2.choices = components_choices
-    logic_form.component1.choices = components_choices
-
-    if logic_form.validate():
-        new_logic = Logic(project_id=obj.id, component_1_id=logic_form.component1.data,
-                          component_2_id=logic_form.component2.data, message_type=logic_form.message_type.data)
-        db.session.add(new_logic)
-        try:
-            db.session.commit()
-            return redirect(url_for('project.view', pid=pid))
-        except:
-            db.session.rollback()
-            flash('creation failed')
-            return redirect(url_for('project.view', pid=pid))
-    flash('invalid logic')
-    return redirect(url_for('project.view', pid=pid))
+    return render_template('project/view.html', project=obj, logic_view=logic_view,components_choices=components_choices, running=running)
 
 
 @project.route('/run/<int:pid>', methods=['POST'])
--- application/forms/project.py
+++ application/forms/project.py
@@ -4,9 +4,3 @@ from wtforms import Form, StringField, SelectField, validators
 class CreateProjectForm(Form):
     name = StringField('Project Name', [validators.DataRequired()])
 
-
-class LogicForm(Form):
-    component1 = SelectField('Component No1', coerce=int, validators=[validators.DataRequired()])
-    component2 = SelectField('Component No2', coerce=int, validators=[validators.DataRequired()])
-    message_type = SelectField('Message Types', choices=[('BasicEvent', 'BasicEvent'), ('type_b', 'type_b')],
-                               validators=[validators.DataRequired()])
