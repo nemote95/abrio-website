cadbe16f847d2b6922c0a7c18dce7ad158daa51e
arian <arian.ah@hotmail.com>
2016-04-22 01:03:27 +0430

    added auth project, fixed chat's project logic bug

--- application/generate_fake.py
+++ application/generate_fake.py
@@ -6,6 +6,7 @@ from application.extensions import db, redis
 
 PROJECT_TEST_TOKEN = '123456'
 PROJECT_CHAT_TOKEN = '123123'
+PROJECT_AUTH_TOKEN = '481516'
 
 
 def generate_fake():
@@ -109,4 +110,48 @@ def generate_development_data():
     logic4.message_type = 'BasicEvent'
 
     db.session.add(logic4)
+    db.session.commit()
+
+    logic5 = Logic()
+    logic5.project_id = project2.id
+    logic5.component_1_id = component3.id
+    logic5.component_2_id = None
+    logic5.message_type = 'BasicEvent'
+
+    db.session.add(logic5)
+    db.session.commit()
+
+    ########### authenticatoin project ###########
+    project3 = Project()
+    project3.name = 'auth'
+    project3.owner = user
+
+    db.session.add(project3)
+    db.session.commit()
+    redis.setnx('abr:'+PROJECT_AUTH_TOKEN, project3.id)
+
+    component4 = Component()
+    component4.name = 'Auth component'
+    component4.deploy_version = str(randint(0, 10))
+    component4.owner = user
+
+    db.session.add(component4)
+    db.session.commit()
+
+    logic6 = Logic()
+    logic6.project_id = project3.id
+    logic6.component_1_id = None
+    logic6.component_2_id = component4.id
+    logic6.message_type = 'BasicEvent'
+
+    db.session.add(logic6)
+    db.session.commit()
+
+    logic7 = Logic()
+    logic7.project_id = project3.id
+    logic7.component_1_id = component4.id
+    logic7.component_2_id = None
+    logic7.message_type = 'BasicEvent'
+
+    db.session.add(logic7)
     db.session.commit()
