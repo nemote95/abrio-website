41a49762b07b160b1056f10e2b2094142bfb5a09
Bardia Heydarinejad <az.bardia13@gmail.com>
2016-05-02 20:57:59 +0430

    Fake generator: Generate auth component and football kaqazi project

--- application/generate_fake.py
+++ application/generate_fake.py
@@ -3,6 +3,7 @@ from models.logic import Logic
 from models.project import Project
 from models.user import User
 from application.extensions import db, redis
+from random import randint
 
 PROJECT_TEST_TOKEN = '123456'
 PROJECT_CHAT_TOKEN = '123123'
@@ -22,7 +23,6 @@ def generate_fake():
 
 
 def generate_development_data():
-    from random import randint
     user = User()
     user.company = 'Abrio'
     user.confirmed = True
@@ -34,9 +34,15 @@ def generate_development_data():
 
     db.session.add(user)
     db.session.commit()
+    generate_project_multiplier_data(user)
+    generate_project_chat_data(user)
+    generate_project_auth_data(user)
+    generate_project_dispatcher_data(user)
 
+
+def generate_project_multiplier_data(user):
     project = Project()
-    project.name = 'test'
+    project.name = 'multiplier'
     project.owner = user
     project.owner_id = user.id
 
@@ -45,7 +51,7 @@ def generate_development_data():
     redis.setnx('abr:' + PROJECT_TEST_TOKEN, project.id)
 
     component1 = Component()
-    component1.name = 'First Component'
+    component1.name = 'multiplier 2'
     component1.deploy_version = str(randint(0, 10))
     component1.owner_id = user.id
 
@@ -53,7 +59,7 @@ def generate_development_data():
     db.session.commit()
 
     component2 = Component()
-    component2.name = 'Second Component'
+    component2.name = 'multiplier 3'
     component2.deploy_version = str(randint(0, 10))
     component2.owner_id = user.id
 
@@ -87,28 +93,85 @@ def generate_development_data():
     db.session.add(logic3)
     db.session.commit()
 
-    project2 = Project()
-    project2.name = 'chat'
-    project2.owner = user
-    project2.owner_id = user.id
 
-    db.session.add(project2)
+def generate_project_chat_data(user):
+    project = Project()
+    project.name = 'chat'
+    project.owner = user
+    project.owner_id = user.id
+
+    db.session.add(project)
     db.session.commit()
-    redis.setnx('abr:' + PROJECT_CHAT_TOKEN, project2.id)
+    redis.setnx('abr:' + PROJECT_CHAT_TOKEN, project.id)
 
-    component3 = Component()
-    component3.name = 'Chat component'
-    component3.deploy_version = str(randint(0, 10))
-    component3.owner_id = user.id
+    component = Component()
+    component.name = 'Chat component'
+    component.deploy_version = str(randint(0, 10))
+    component.owner_id = user.id
 
-    db.session.add(component3)
+    db.session.add(component)
     db.session.commit()
 
-    logic4 = Logic()
-    logic4.project_id = project2.id
-    logic4.component_1_id = None
-    logic4.component_2_id = component3.id
-    logic4.message_type = 'BasicEvent'
+    logic1 = Logic()
+    logic1.project_id = project.id
+    logic1.component_1_id = None
+    logic1.component_2_id = component.id
+    logic1.message_type = 'BasicEvent'
 
-    db.session.add(logic4)
+    db.session.add(logic1)
+    db.session.commit()
+
+    logic2 = Logic()
+    logic2.project_id = project.id
+    logic2.component_1_id = component.id
+    logic2.component_2_id = None
+    logic2.message_type = 'BasicEvent'
+
+    db.session.add(logic1)
+    db.session.commit()
+
+
+def generate_project_auth_data(user):
+    component = Component()
+    component.name = 'auth'
+    component.deploy_version = str(randint(0, 10))
+    component.owner_id = user.id
+    db.session.add(component)
+    db.session.commit()
+
+
+def generate_project_dispatcher_data(user):
+    project = Project()
+    project.name = 'football kaghazi'
+    project.owner = user
+    project.owner_id = user.id
+
+    db.session.add(project)
+    db.session.commit()
+    redis.setnx('abr:' + PROJECT_CHAT_TOKEN, project.id)
+
+    component = Component()
+    component.name = 'One to one dispatcher'
+    component.deploy_version = str(randint(0, 10))
+    component.owner_id = user.id
+
+    db.session.add(component)
+    db.session.commit()
+
+    logic5 = Logic()
+    logic5.project_id = project.id
+    logic5.component_1_id = None
+    logic5.component_2_id = component.id
+    logic5.message_type = 'BasicEvent'
+
+    db.session.add(logic5)
+    db.session.commit()
+
+    logic6 = Logic()
+    logic6.project_id = project.id
+    logic6.component_1_id = component.id
+    logic6.component_2_id = None
+    logic6.message_type = 'BasicEvent'
+
+    db.session.add(logic6)
     db.session.commit()
