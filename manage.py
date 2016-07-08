d76181ef7516d1529251b04bc3525cddf2e00ac9
nemote95 <nemote95@gmail.com>
2016-07-08 18:15:46 +0430

    change host to 0.0.0.0

--- manage.py
+++ manage.py
@@ -1,13 +1,14 @@
 # python imports
 import os
 # flask imports
-from flask.ext.script import Manager, Shell
+from flask.ext.script import Manager, Shell,Server
 # project imports
 from application import create_app
 from application.database import manager as database_manager
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
+server = Server(host="0.0.0.0", port=9000)
 
 
 def make_shell_context():
@@ -15,6 +16,7 @@ def make_shell_context():
 
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command("runserver", Server(host="0.0.0.0", port=8080))
 manager.add_command("database", database_manager)
 
 
